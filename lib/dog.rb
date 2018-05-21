e1977eb510c625afa2b7f608dfd5e6425cb58311
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-05-21 04:17:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,10 @@
 class Dog 
   def initialize(dog_name, dog_breed = "Mutt")
     @breed = dog_name
-  end
-  def 
 end
+  def dog=(dog_breed)
+    @breed = dog_name
+end
+  def dog
+    @breed
+  end
